ae564f92c2f5e2ccb6dc5cc9f3c74b7556d2c06b
EslamKhial1997 <eslamkhial1997@gmail.com>
2024-06-08 02:54:18 +0300

    auth

---
+++ .history/Routes/RoutesAuth_20240608024525.js
@@ -0,0 +1,35 @@
+const { Router } = require("express");
+
+const {
+  LoginValidator,
+} = require("../Resuble/AuthvalidatorError");
+const {
+  SingUp,
+  Login,
+  forgetPassword,
+  restCodeSent,
+  restNewPassword,
+} = require("../Service/AuthService");
+const { verifyRegister } = require("../Service/UsersService");
+const { createUsersValidator } = require("../Resuble/UsersvalidatorError");
+
+// const {
+//   createUsersValidator,
+//   getOneUserValidator,
+//   updateOneUserValidator,
+//   deleteOneUserValidator,
+//   UpdateUserPassword,
+// } = require("../Resuble/UsersvalidatorError");
+
+const Routes = Router();
+
+Routes.route("/signup").post(createUsersValidator, SingUp);
+Routes.route("/verifycode").post(verifyRegister);
+Routes.route("/login").post(LoginValidator, Login);
+Routes.post("/forgetPassword", forgetPassword);
+Routes.post("/restCode", restCodeSent);
+Routes.put("/setNewPassword", restNewPassword("password"));
+Routes.route("/:id").get(getOne, getUser)
+//   .put(uploadImage,resizeImageUsers,updateOneUserValidator, updateUser)
+//   .delete(deleteOneUserValidator, deleteUser);
+module.exports = Routes;
---
+++ .history/Routes/RoutesUsers_20240608015852.js
@@ -0,0 +1,65 @@
+const { Router } = require("express");
+const { createUsersValidator } = require("../Resuble/UsersvalidatorError");
+const {
+  verifyRegister,
+  createUsers,
+  getUsers,
+  //   updateUser,
+  //   deleteUser,
+  getUser,
+  //   updatePassword,
+  //   uploadImage,
+  //   resizeImageUsers,
+  updateLoggedUserPassword,
+  getLoggedUserData,
+  //   updateLoggedUserData,
+
+  //   updateUserRole,
+  //   updateUserStatus,
+} = require("../Service/UsersService");
+const { protect, allowedTo } = require("../Service/AuthService");
+// const {
+//   createUsersValidator,
+//   getOneUserValidator,
+//   updateOneUserValidator,
+//   deleteOneUserValidator,
+//   UpdateUserPassword,
+//   updateLoggedUserValidator,
+//   LoginDashboardValidator,
+// } = require("../Resuble/UsersvalidatorError");
+
+
+const Routes = Router();
+
+// Only Access the Logged Users
+Routes.use(protect);
+Routes.get("/getMe", getLoggedUserData, getUser);
+// Routes.put(
+//   "/updateMe",
+//   uploadImage,
+//   resizeImageUsers,
+//   updateLoggedUserValidator,
+//   updateLoggedUserData
+// );
+// Routes.put("/changePassword/:id", UpdateUserPassword, updatePassword);
+Routes.put("/changeUserPassword", updateLoggedUserPassword);
+// Routes.post("/forgetPassword", forgetPassword);
+// Routes.post("/restCode", restCodeSent);
+// Routes.patch("/restNewPasswordDashboard", restNewPassword("passwordDB"));
+// // Only Access the Admin
+// Routes.use(allowedTo("admin", "manger"));
+// Routes.route("/createpasswordDashboard").post(createpasswordDashboard);
+// Routes.post("/loginDashboard", LoginDashboardValidator, LoginDashboard);
+// Routes.route("/updatepasswordDashboard/:id").put(updatePasswordDashboard);
+// Routes.route("/updateUseRole/:id").put(updateUserRole);
+// Routes.route("/updateUserStatus/:id").put(updateUserStatus);
+
+Routes.use(allowedTo("admin", "manager"));
+Routes.route("/").post(createUsersValidator, createUsers).get(getUsers);
+Routes.route("/verifycode").post(verifyRegister);
+
+// Routes.route("/:id")
+//   .get(getOneUserValidator, getUser)
+//   .patch(uploadImage, resizeImageUsers, updateOneUserValidator, updateUser)
+//   .delete(deleteOneUserValidator, deleteUser);
+module.exports = Routes;
