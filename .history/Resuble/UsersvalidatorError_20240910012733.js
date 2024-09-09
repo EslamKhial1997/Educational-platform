77aaf29ca58d993f48956e48988ba8a95e4e13d8
EslamKhial1997 <eslamkhial1997@gmail.com>
2024-09-10 01:35:37 +0300

    last update 9/9/2024

---
+++ .history/Resuble/UsersvalidatorError_20240910012733.js
@@ -0,0 +1,166 @@
+const { check, body } = require("express-validator");
+const { default: slugify } = require("slugify");
+const bcrypt = require("bcryptjs");
+const {
+  MiddlewareValidator,
+} = require("../Middleware/MiddlewareValidatorError");
+const createUsersModel = require("../Modules/createUsers");
+const createTeachersModel = require("../Modules/createTeacher");
+const ApiError = require("./ApiErrors");
+
+// const createUsersModel = require("../modules/createUsers");
+
+exports.createUsersValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("is required Name")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+
+  check("password")
+    .notEmpty()
+    .withMessage("is required Password")
+    .withMessage("To Shoort Password To CreateUser Validator"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("To Shoort Password To CreateUser Validator")
+    .custom((val, { req }) => {
+      if (val !== req.body.passwordConfirm) {
+        return Promise.reject(new Error("Confirm Password No Match"));
+      }
+      return true;
+    }),
+  check("passwordConfirm")
+    .notEmpty()
+    .withMessage("Password confirmation required"),
+
+  check("email").notEmpty().withMessage("is required E-mail"),
+  check("email")
+    .isEmail()
+    .withMessage("Must be at E-mail Address")
+    .custom((val) =>
+      createUsersModel.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("ايميل المستخدم موجود بالفعل"));
+        }
+      })
+    ),
+  check("email")
+    .isEmail()
+    .withMessage("Must be at E-mail Address")
+    .custom((val) =>
+      createTeachersModel.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("ايميل المستخدم موجود بالفعل"));
+        }
+      })
+    ),
+
+  check("phone")
+    .notEmpty()
+    .isMobilePhone(["ar-EG", "ar-SA"])
+    .custom((val, { req }) => {
+      req.body.wallet = val;
+      return true;
+    })
+    .withMessage("Invailable phone number EG , SA Number Only accepted")
+    .custom((val) =>
+      createUsersModel.findOne({ phone: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("رقم الموبايل مستخدم"));
+        }
+      })
+    ),
+  MiddlewareValidator,
+];
+
+exports.getOneUserValidator = [
+  check("id").isMongoId().withMessage("Sorry ID Not Available To get"),
+  MiddlewareValidator,
+];
+exports.deleteOneUserValidator = [
+  check("id").isMongoId().withMessage("Sorry ID Not Available To delete"),
+  MiddlewareValidator,
+];
+exports.updateOneUserValidator = [
+  check("id").isMongoId().withMessage("Sorry ID Not Available To Update"),
+  check("name")
+    .optional()
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check("image").optional(),
+  MiddlewareValidator,
+];
+exports.deleteOneUserValidator = [
+  check("id").isMongoId().withMessage("Sorry ID Not Available To Delete"),
+  MiddlewareValidator,
+];
+exports.UpdateUserPassword = [
+  check("currentPasword")
+    .notEmpty()
+    .withMessage("you Must enter Old password "),
+  check("passwordConfirm")
+    .notEmpty()
+    .withMessage("you Must enter belong password "),
+  check("password")
+    .notEmpty()
+    .withMessage("you Must enter password ")
+    .custom(async (val, { req }) => {
+      const user = await createUsersModel.findById(req.params.id);
+
+      if (!user) {
+        throw new ApiError("الباسورد القديم غير صحيح", 500);
+      }
+      const iscorrectPassword = await bcrypt.compare(
+        req.body.currentPasword,
+        user.password
+      );
+      if (!iscorrectPassword) {
+        throw new ApiError("الباسورد القديم غير صحيح");
+      }
+      if (val !== req.body.passwordConfirm) {
+        throw new ApiError("in Correct passwordConfirm", 500);
+      }
+    }),
+  MiddlewareValidator,
+];
+exports.UpdatePaidToTeacherValidation = [
+  // التحقق من أن المعرف هو معرف Mongo صالح
+  check("id")
+    .isMongoId()
+    .withMessage("Sorry ID Not Available To Update")
+    .custom((val) =>
+      createTeachersModel.findOne({ _id: val }).then((teacher) => {
+        if (!teacher) {
+          return Promise.reject(new Error("المدرس غير موجود"));
+        }
+      })
+    ),
+
+  // التحقق من صحة قيمة النقاط
+  check("point")
+    .isNumeric()
+    .withMessage("يجب أن تكون النقاط رقمية")
+    .custom(async (val, { req }) => {
+      const teacher = await createTeachersModel.findOne({ _id: req.params.id });
+      if (!teacher) {
+        return Promise.reject(new Error("المدرس غير موجود"));
+      }
+
+      // التحقق مما إذا كانت النقاط المطلوب خصمها أكبر من نقاط المعلم
+      if (val > teacher.point) {
+        return Promise.reject(
+          new Error("النقاط المطلوب خصمها أكبر من نقاط المدرس الحالية")
+        );
+      }
+
+      return true;
+    }),
+
+  // استدعاء MiddlewareValidator للتحقق من الأخطاء
+  MiddlewareValidator,
+];
---
+++ .history/Service/LectureService_20240903160559.js
@@ -0,0 +1,58 @@
+const factory = require("./FactoryHandler");
+const fs = require("fs");
+const axios = require("axios");
+const createLecturesModel = require("../Modules/createAlecture");
+const expressAsyncHandler = require("express-async-handler");
+const createSectionModel = require("../Modules/createSection");
+const createTeachersModel = require("../Modules/createTeacher");
+
+exports.resizeImage = expressAsyncHandler(async (req, res, next) => {
+  if (req.file) {
+    req.body.pdf = req.file.filename;
+  }
+
+  next();
+});
+exports.createLectures = expressAsyncHandler(async (req, res) => {
+  try {
+    const section = await createSectionModel
+      .findById(req.body.section)
+      .then((e) => e.class.teacher._id);
+    const teacherKey = await createTeachersModel
+      .findById(section)
+      .then((e) => e );
+
+
+    await axios
+      .post(
+        `https://video.bunnycdn.com/library/${teacherKey.libraryID}/videos`,
+        { title: req.body.lecture },
+        {
+          headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+            AccessKey: teacherKey.key,
+          },
+        }
+      )
+      .then(async (response) => {
+        req.body.bunny = {
+          videoLibraryId: response.data.videoLibraryId,
+          guid: response.data.guid,
+          key: teacherKey.key,
+        };
+
+        const createDoc = await createLecturesModel.create(req.body);
+
+        res.status(201).json({ data: createDoc });
+      });
+  } catch (error) { 
+    console.error("Error uploading file:", error.message);
+    res.status(500).send({ error: "Failed to upload video" });
+  }
+});
+
+exports.getLectures = factory.getAll(createLecturesModel);
+exports.getLecture = factory.getOne(createLecturesModel);
+exports.updateLecture = factory.updateOne(createLecturesModel, "lecture");
+exports.deleteLecture = factory.deleteOne(createLecturesModel, "lecture");
